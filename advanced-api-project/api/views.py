b96b1e18418efb98421b65efedc2aa004afc203a
Akong Rodney <rodneyakong@gmail.com>
2025-08-10 21:04:34 +0100

    add advanced query capabilities to your existing views that manage the Book model.

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,7 @@
 # api/views.py
 from rest_framework import generics, filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from django_filters import rest_framework  # <-- checker wants this exact import
+from django_filters import rest_framework
 from .models import Book
 from .serializers import BookSerializer
 
@@ -15,11 +15,11 @@ class BookListView(generics.ListAPIView):
     filter_backends = [rest_framework.DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
 
     # Filtering fields
-    filterset_fields = ['title', 'author', 'published_date']
+    filterset_fields = ['title', 'author', 'publication_year']
 
     # Searching fields
     search_fields = ['title', 'author']
 
     # Ordering fields
-    ordering_fields = ['title', 'published_date', 'pages']
+    ordering_fields = ['title', 'publication_year', 'pages']
     ordering = ['title']  # default ordering
