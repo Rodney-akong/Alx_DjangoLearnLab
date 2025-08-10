946b34f995d8c6d2b2aafab66519ebdec92a7b44
Akong Rodney <rodneyakong@gmail.com>
2025-08-10 20:40:48 +0100

    creating and configuring custom views using Django REST Framework’s powerful generic views and mixins.

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,9 +1,14 @@
 # api/urls.py
 from django.urls import path
-from .views import BookListCreateView, BookDetailView
+from .views import (
+    BookListView, BookDetailView,
+    BookCreateView, BookUpdateView, BookDeleteView
+)
 
 urlpatterns = [
-    path('books/', BookListCreateView.as_view(), name='book-list'),
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+    path('books/', BookListView.as_view(), name='book-list'),  # GET all books
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),  # GET one book
+    path('books/create/', BookCreateView.as_view(), name='book-create'),  # POST create new book
+    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),  # PUT/PATCH update book
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),  # DELETE remove book
 ]
-
