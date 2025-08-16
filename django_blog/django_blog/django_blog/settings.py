706c9ddae96b432139597df9db4e9ac31f7dcaeb
Akong Rodney <rodneyakong@gmail.com>
2025-08-16 21:59:27 +0100

    setting up the Django environment, creating the primary Django application, and configuring the basic models necessary for a blogging platform.

--- django_blog/django_blog/django_blog/settings.py
+++ django_blog/django_blog/django_blog/settings.py
@@ -55,10 +55,11 @@ ROOT_URLCONF = 'django_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [],   # later we can add global template dirs
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
